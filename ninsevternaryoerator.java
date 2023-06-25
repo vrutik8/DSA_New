7c3bc273826ebab87f42e5d90c96eb3552ca734c
vrutik <surtivrutik18@gmail.com>
2023-06-25 18:31:32 +0530

    first commit

---
+++ ninsevternaryoerator.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ninsevternaryoerator{
+    public static void main(String args[]){
+    // int n = 3;
+    // String type = (n%2==0) ? "even " : "odd";
+    // System.out.println(type);
+    int type = (7>9) ? 7 : 9  ;
+    System.out.println(type);
+
+}
+}
---
+++ ninsixlarestofthnos.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ninsixlarestofthnos{
+    public static void main(String[] args) {
+        int a = 1000 , b= 8900 , c = 60000;
+
+        if((a>b) && (a>c) ){
+            System.out.println("is largest is a");
+        }
+        else if(b>c){
+            System.out.println("is largest is b");
+        }
+        else{
+            System.out.println( " c is largest of all");
+        }
+    }
+}
