3885c5007c1bec0869fac4c34b05f3e4a81af109
vrutik <surtivrutik18@gmail.com>
2023-06-26 21:52:42 +0530

    last commit

---
+++ tenloops.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class tenloops {
+    public static void main(String args[]){
+        int counter = 0;
+        while(counter<5){
+            System.out.println("Hello World !");
+            counter++;
+        }
+        System.out.println("printed 100 times");
+    }
+    
+}
---
+++ tenthrnaturalnumber.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class tenthrnaturalnumber {
+    public static void main(String args[]){
+            int n = 1;
+            while(n<101){
+                System.out.print( n + " ");
+                n++;
+            }
+    }
+}
