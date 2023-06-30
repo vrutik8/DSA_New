03d5a7843f9050c48885a8d35f1c46e29f0054a6
vrutik <surtivrutik18@gmail.com>
2023-06-30 15:21:54 +0530

    squarepattern

---
+++ tensev.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class tensev {
+    public static void main(String args[]) {
+        // for (int i = 1; i <= 3; i++) {
+
+        // System.out.println("****");
+        // }
+        int i = 1;
+        while (i <= 3) {
+            System.out.println("****");
+            i++;
+        }
+    }
+}
