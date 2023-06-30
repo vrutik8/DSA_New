03c05054aee19560dd2b1670f8dde2e066c9b21b
vrutik <surtivrutik18@gmail.com>
2023-06-30 15:05:50 +0530

    second commit

---
+++ tensixfor.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class tensixfor {
+    public static void main(String args[]) {
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Hello world");
+        }
+        System.out.println("done");
+    }
+}
